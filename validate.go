89c486476ba92c15cf66324cced6d681d3bf5d68
Adam Pickering <adam@insidedesk.com>
2021-09-25 12:16:20 -0600

    Add unit test for isPalindrome

--- validate.go
+++ validate.go
@@ -1,48 +1,72 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
+	"strings"
 
-	onelog "github.com/francoispqt/onelog"
+	//onelog "github.com/francoispqt/onelog"
 	"github.com/kubewarden/gjson"
 	kubewarden "github.com/kubewarden/policy-sdk-go"
 )
 
 func validate(payload []byte) ([]byte, error) {
-	settings, err := NewSettingsFromValidationReq(payload)
-	if err != nil {
+	// ensure we got valid json
+	if !gjson.ValidBytes(payload) {
 		return kubewarden.RejectRequest(
-			kubewarden.Message(err.Error()),
-			kubewarden.Code(400))
+			kubewarden.Message("Not a valid JSON document"),
+			kubewarden.Code(400),
+		)
 	}
 
-	data := gjson.GetBytes(
-		payload,
-		"request.object.metadata.name")
+	// build Settings struct from payload
+	//settings, err := NewSettingsFromValidationReq(payload)
+	//if err != nil {
+	//	return kubewarden.RejectRequest(
+	//		kubewarden.Message(err.Error()),
+	//		kubewarden.Code(400))
+	//}
 
-	if !data.Exists() {
-		logger.Warn("cannot read object name from metadata: accepting request")
-		return kubewarden.AcceptRequest()
-	}
-	name := data.String()
+	//data := gjson.GetBytes(
+	//	payload,
+	//	"request.object.metadata.labels")
 
-	logger.DebugWithFields("validating ingress object", func(e onelog.Entry) {
-		namespace := gjson.GetBytes(payload, "request.object.metadata.namespace").String()
-		e.String("name", name)
-		e.String("namespace", namespace)
-	})
+	//if !data.Exists() {
+	//	onelog.Warn("cannot read labels from metadata: accepting request")
+	//	return kubewarden.AcceptRequest()
+	//}
+	//name := data.String()
 
-	if settings.DeniedNames.Contains(name) {
-		logger.InfoWithFields("rejecting ingress object", func(e onelog.Entry) {
-			e.String("name", name)
-			e.String("denied_names", settings.DeniedNames.String())
-		})
+	//onelog.DebugWithFields("validating ingress object", func(e onelog.Entry) {
+	//	namespace := gjson.GetBytes(payload, "request.object.metadata.namespace").String()
+	//	e.String("name", name)
+	//	e.String("namespace", namespace)
+	//})
 
-		return kubewarden.RejectRequest(
-			kubewarden.Message(
-				fmt.Sprintf("The '%s' name is on the deny list", name)),
-			kubewarden.NoCode)
-	}
+	//if settings.DeniedNames.Contains(name) {
+	//	onelog.InfoWithFields("rejecting ingress object", func(e onelog.Entry) {
+	//		e.String("name", name)
+	//		e.String("denied_names", settings.DeniedNames.String())
+	//	})
+
+	//	return kubewarden.RejectRequest(
+	//		kubewarden.Message(
+	//			fmt.Sprintf("The '%s' name is on the deny list", name)),
+	//		kubewarden.NoCode)
+	//}
 
 	return kubewarden.AcceptRequest()
+}
+
+
+func isPalindrome(input string) bool {
+	input_length := len([]rune(input))
+	iterations := input_length / 2
+	lowered_input := strings.ToLower(input)
+	lowered_runes := []rune(lowered_input)
+	for i := 0; i < iterations; i++ {
+		if lowered_runes[i] != lowered_runes[input_length - (1 + i)] {
+			return false
+		}
+	}
+	return true
 }
--- validate_test.go
+++ validate_test.go
@@ -1,93 +1,125 @@
 package main
 
 import (
-	"encoding/json"
+	//"encoding/json"
 	"testing"
 
-	"github.com/deckarep/golang-set"
-	kubewarden_testing "github.com/kubewarden/policy-sdk-go/testing"
+	//"github.com/deckarep/golang-set"
+	//kubewarden_testing "github.com/kubewarden/policy-sdk-go/testing"
 )
 
-func TestEmptySettingsLeadsToApproval(t *testing.T) {
-	settings := Settings{}
+//func TestEmptySettingsLeadsToApproval(t *testing.T) {
+//	settings := Settings{}
+//
+//	payload, err := kubewarden_testing.BuildValidationRequest(
+//		"test_data/ingress.json",
+//		&settings)
+//	if err != nil {
+//		t.Errorf("Unexpected error: %+v", err)
+//	}
+//
+//	responsePayload, err := validate(payload)
+//	if err != nil {
+//		t.Errorf("Unexpected error: %+v", err)
+//	}
+//
+//	var response kubewarden_testing.ValidationResponse
+//	if err := json.Unmarshal(responsePayload, &response); err != nil {
+//		t.Errorf("Unexpected error: %+v", err)
+//	}
+//
+//	if response.Accepted != true {
+//		t.Error("Unexpected rejection")
+//	}
+//}
+//
+//func TestApproval(t *testing.T) {
+//	settings := Settings{
+//		DeniedNames: mapset.NewThreadUnsafeSetFromSlice([]interface{}{"foo", "bar"}),
+//	}
+//
+//	payload, err := kubewarden_testing.BuildValidationRequest(
+//		"test_data/ingress.json",
+//		&settings)
+//	if err != nil {
+//		t.Errorf("Unexpected error: %+v", err)
+//	}
+//
+//	responsePayload, err := validate(payload)
+//	if err != nil {
+//		t.Errorf("Unexpected error: %+v", err)
+//	}
+//
+//	var response kubewarden_testing.ValidationResponse
+//	if err := json.Unmarshal(responsePayload, &response); err != nil {
+//		t.Errorf("Unexpected error: %+v", err)
+//	}
+//
+//	if response.Accepted != true {
+//		t.Error("Unexpected rejection")
+//	}
+//}
+//
+//func TestRejection(t *testing.T) {
+//	settings := Settings{
+//		DeniedNames: mapset.NewThreadUnsafeSetFromSlice([]interface{}{"foo", "tls-example-ingress"}),
+//	}
+//
+//	payload, err := kubewarden_testing.BuildValidationRequest(
+//		"test_data/ingress.json",
+//		&settings)
+//	if err != nil {
+//		t.Errorf("Unexpected error: %+v", err)
+//	}
+//
+//	responsePayload, err := validate(payload)
+//	if err != nil {
+//		t.Errorf("Unexpected error: %+v", err)
+//	}
+//
+//	var response kubewarden_testing.ValidationResponse
+//	if err := json.Unmarshal(responsePayload, &response); err != nil {
+//		t.Errorf("Unexpected error: %+v", err)
+//	}
+//
+//	if response.Accepted != false {
+//		t.Error("Unexpected approval")
+//	}
+//
+//	expected_message := "The 'tls-example-ingress' name is on the deny list"
+//	if response.Message != expected_message {
+//		t.Errorf("Got '%s' instead of '%s'", response.Message, expected_message)
+//	}
+//}
 
-	payload, err := kubewarden_testing.BuildValidationRequest(
-		"test_data/ingress.json",
-		&settings)
-	if err != nil {
-		t.Errorf("Unexpected error: %+v", err)
-	}
-
-	responsePayload, err := validate(payload)
-	if err != nil {
-		t.Errorf("Unexpected error: %+v", err)
-	}
-
-	var response kubewarden_testing.ValidationResponse
-	if err := json.Unmarshal(responsePayload, &response); err != nil {
-		t.Errorf("Unexpected error: %+v", err)
-	}
-
-	if response.Accepted != true {
-		t.Error("Unexpected rejection")
-	}
+type palindromeTestCase struct {
+	Value string
+	ShouldReturn bool
 }
 
-func TestApproval(t *testing.T) {
-	settings := Settings{
-		DeniedNames: mapset.NewThreadUnsafeSetFromSlice([]interface{}{"foo", "bar"}),
+func TestIsPalindrome(t *testing.T) {
+	test_cases := []palindromeTestCase{
+		palindromeTestCase{Value: "racecar", ShouldReturn: true},
+		palindromeTestCase{Value: "racecat", ShouldReturn: false},
+		palindromeTestCase{Value: "racebar", ShouldReturn: false},
+		palindromeTestCase{Value: "pacecar", ShouldReturn: false},
+		palindromeTestCase{Value: "raccar", ShouldReturn: true},
+		palindromeTestCase{Value: "racear", ShouldReturn: false},
+		palindromeTestCase{Value: "naccar", ShouldReturn: false},
+		palindromeTestCase{Value: "asdf", ShouldReturn: false},
+		palindromeTestCase{Value: "Racecar", ShouldReturn: true},
+		palindromeTestCase{Value: "1racecar1", ShouldReturn: true},
+		palindromeTestCase{Value: "rac#e#car", ShouldReturn: true},
+		palindromeTestCase{Value: "rαcecαr", ShouldReturn: true},
+		palindromeTestCase{Value: "", ShouldReturn: true},
+		palindromeTestCase{Value: "x", ShouldReturn: true},
+		palindromeTestCase{Value: "xx", ShouldReturn: true},
 	}
 
-	payload, err := kubewarden_testing.BuildValidationRequest(
-		"test_data/ingress.json",
-		&settings)
-	if err != nil {
-		t.Errorf("Unexpected error: %+v", err)
-	}
-
-	responsePayload, err := validate(payload)
-	if err != nil {
-		t.Errorf("Unexpected error: %+v", err)
-	}
-
-	var response kubewarden_testing.ValidationResponse
-	if err := json.Unmarshal(responsePayload, &response); err != nil {
-		t.Errorf("Unexpected error: %+v", err)
-	}
-
-	if response.Accepted != true {
-		t.Error("Unexpected rejection")
-	}
-}
-
-func TestRejection(t *testing.T) {
-	settings := Settings{
-		DeniedNames: mapset.NewThreadUnsafeSetFromSlice([]interface{}{"foo", "tls-example-ingress"}),
-	}
-
-	payload, err := kubewarden_testing.BuildValidationRequest(
-		"test_data/ingress.json",
-		&settings)
-	if err != nil {
-		t.Errorf("Unexpected error: %+v", err)
-	}
-
-	responsePayload, err := validate(payload)
-	if err != nil {
-		t.Errorf("Unexpected error: %+v", err)
-	}
-
-	var response kubewarden_testing.ValidationResponse
-	if err := json.Unmarshal(responsePayload, &response); err != nil {
-		t.Errorf("Unexpected error: %+v", err)
-	}
-
-	if response.Accepted != false {
-		t.Error("Unexpected approval")
-	}
-
-	expected_message := "The 'tls-example-ingress' name is on the deny list"
-	if response.Message != expected_message {
-		t.Errorf("Got '%s' instead of '%s'", response.Message, expected_message)
+	for _, test_case := range test_cases {
+		result := isPalindrome(test_case.Value)
+		if test_case.ShouldReturn != result {
+			t.Errorf("%s returned %t instead of %t", test_case.Value, result, test_case.ShouldReturn)
+		}
 	}
 }
